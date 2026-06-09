bda4497af623a6fdd21a23159c3439ea56bc5e48
lenadweb <lenadweber@gmail.com>
2026-06-09 20:10:12 +0700

    fix svg viewbox

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,25 @@ module.exports = (env) => ({
             },
             {
                 test: /\.svg$/,
-                use: ['@svgr/webpack'],
+                use: [
+                    {
+                        loader: '@svgr/webpack',
+                        options: {
+                            svgoConfig: {
+                                plugins: [
+                                    {
+                                        name: 'preset-default',
+                                        params: {
+                                            overrides: {
+                                                removeViewBox: false,
+                                            },
+                                        },
+                                    },
+                                ],
+                            },
+                        },
+                    },
+                ],
             },
             {
                 test: /\.css$/i,
