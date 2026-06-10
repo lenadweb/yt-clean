013f2427e593fc87abc85d916a149132dcf918f3
lenadweb <lenadweber@gmail.com>
2026-06-10 19:40:23 +0700

    remove unused styles and refactor code

--- src/shared/stylesBuilder/index.ts
+++ src/shared/stylesBuilder/index.ts
@@ -1,90 +1,26 @@
 import { ElementActions, FeatureAction } from 'src/shared/types/config';
 import { FEATURES, SECTIONS } from 'src/shared/featureConfig';
-import { getAttr } from 'src/shared/utils/getAttr';
 
 const HIDE_STYLE = 'display: none !important;';
 
-const RIGHT_MODE_ATTR = getAttr('right-comment-mode');
-
-const RIGHT_MODE_STYLES = `
-[${RIGHT_MODE_ATTR}] #columns {
-    flex-direction: column;
-    display: grid;
-    grid-template-columns: 1fr;
-}
-
-[${RIGHT_MODE_ATTR}] #primary {
-    display: flex;
-    flex-direction: row;
-    max-width: none!important;
-}
-
-[${RIGHT_MODE_ATTR}] #primary-inner {
-    display: flex;
-}
-
-[${RIGHT_MODE_ATTR}] #below {
-    max-height: 504px;
-    overflow: auto;
-    margin-left: 32px;
-}
-
-[${RIGHT_MODE_ATTR}] #secondary {
-    width: 100%!important;
-}
-
-[${RIGHT_MODE_ATTR}] #columns {
-    display: block!important;
-}
-
-[${RIGHT_MODE_ATTR}] #primary > #primary-inner {
-    flex: 1 1 100%;
-}
-
-[${RIGHT_MODE_ATTR}] #secondary-inner #related #items ytd-item-section-renderer > #contents {
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(370px, 1fr));
-    gap: 24px;
-    padding: 24px;
-}
-
-[${RIGHT_MODE_ATTR}] #secondary #items .yt-related-chip-cloud-renderer #scroll-container {
-    width: 100%;
-}
-`;
-
-const DEFAULT_STYLES = `
-.${getAttr('hide-class')} {
-    ${HIDE_STYLE}
-}
-
-${RIGHT_MODE_STYLES}
-`;
-
 const buildActionStyles = (action: FeatureAction): string[] => {
-    if (action.action === ElementActions.hide) {
-        return (
-            action.selectors?.map(
+    switch (action.action) {
+        case ElementActions.hide:
+            return action.selectors.map(
                 (selector) => `[${action.attr}] ${selector} { ${HIDE_STYLE} }`
-            ) ?? []
-        );
+            );
+        case ElementActions.customStyles:
+            return action.customStyles.map(
+                (style) => `[${action.attr}] ${style}`
+            );
+        default:
+            return [];
     }
-
-    if (action.action === ElementActions.customStyles) {
-        return (
-            action.customStyles?.map((item) => `[${action.attr}] ${item}`) ?? []
-        );
-    }
-
-    return [];
 };
 
-const getSectionActions = (): FeatureAction[] =>
-    SECTIONS.flatMap((section) => section.onFullGroupEnabledActions ?? []);
+const allActions = [
+    ...FEATURES.flatMap((feature) => feature.actions),
+    ...SECTIONS.flatMap((section) => section.onFullGroupEnabledActions ?? []),
+];
 
-export const styles = [...FEATURES.flatMap(({ actions }) => actions)]
-    .concat(getSectionActions())
-    .flatMap(buildActionStyles)
-    .filter(Boolean)
-    .join('\n')
-    .concat(`\n${DEFAULT_STYLES}`);
+export const styles = allActions.flatMap(buildActionStyles).join('\n');
