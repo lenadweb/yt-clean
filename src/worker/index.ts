6466d0b988409cb07b4aada50cfd9e7934a9a0d3
lenadweb <lenadweber@gmail.com>
2026-08-31 19:46:53 +0700

    add onboarding page creation on install

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,5 +1,13 @@
 import { isOpera } from 'src/shared/utils/browser';
 
+chrome.runtime.onInstalled.addListener(({ reason }) => {
+    if (reason !== 'install') return;
+
+    chrome.tabs.create({
+        url: chrome.runtime.getURL('onboarding.html'),
+    });
+});
+
 if (!isOpera() && chrome.sidePanel) {
     chrome.sidePanel
         .setPanelBehavior({ openPanelOnActionClick: true })
